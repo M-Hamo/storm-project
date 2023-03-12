75f714b5253211acd5b8fb1ebfe3df42e0cb3fed
Mohamed Eldeeb <58711682+M-Hamo@users.noreply.github.com>
2023-03-12 11:45:38 +0200

    ...

--- src/app/main-layout/main-layout-routing.module.ts
+++ src/app/main-layout/main-layout-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'no-assessments',
+        redirectTo: 'assessments',
         pathMatch: 'full',
       },
       {
